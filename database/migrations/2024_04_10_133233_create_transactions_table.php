7676ec9589ef2c1f827b049ccac6464e782e870c
mando <143672248+Sumandosilalahi2929@users.noreply.github.com>
2024-04-11 22:04:46 +0700

    feat: add seeder factory in data transaction and repair bug transaction

--- database/migrations/2024_04_10_133233_create_transactions_table.php
+++ database/migrations/2024_04_10_133233_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('listing_id')->constrained();
             $table->date('start_date');
             $table->date('end_date');
             $table->integer('price_per_day')->unsigned()->default(0);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\Listing;
 use App\Models\User;
+use App\Models\Listing;
+use App\Models\Transaction;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,13 +17,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'Admin@gmail.com',
             'role' => 'admin',
         ]);
+        
+        $users = User::factory(10)->create();
         $listings = Listing::factory(10)->create();
+
+        Transaction::factory(10)
+        ->state(
+            new Sequence(
+                fn(Sequence $sequence) => [
+                    'user_id' => $users->random(),
+                    'listing_id' => $listings->random(),
+                ]
+            )
+        )->create();
     }
 }
